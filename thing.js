374f8afa39470ae9dd074c96b58ea979692ecddb
Drew Reese <drew.w.reese@gmail.com>
2016-11-10 15:38:57 -0800

    Various Performance Improvements
    Radius is now a field in Thing and only updated after collisions.
    Any other calculations involving radius but doesn't directly need the
    radius value now just uses radius squared (this saves an unnecessary
    sqrt calc when were just squaring that value back).
    Updated boundary reflection so restitution (bounce factor) is applied
    correctly (i.e. to both axis).

--- thing.js
+++ thing.js
@@ -15,6 +15,36 @@ var HISTORY_ALPHA_CUTOFF_THRESHOLD = 0.05;
 var SHOW_HISTORY = true;
 
 
+function ThingParticleSystem(parent) {
+    ParticleSystem.call(this);
+    this.parent = parent;
+};
+
+ThingParticleSystem.prototype = Object.create(ParticleSystem.prototype);
+ThingParticleSystem.prototype.constructor = ThingParticleSystem;
+
+ThingParticleSystem.prototype.addParticle = function() {
+    // ParticleSystem.prototype.addParticle.call(this, this.parentThing.pos);
+    particle = new ThingParticle(this.parent.pos.copy());
+    this.particles.push(particle);
+};
+
+function ThingParticle(position) {
+	Particle.call(this, position.copy());
+};
+
+ThingParticle.prototype = Object.create(Particle.prototype);
+ThingParticle.prototype.constructor = Particle;
+
+ThingParticle.prototype.display = function() {
+	push();
+	var c = 255.0 - this.lifespan;
+	stroke(c, c, c, this.lifespan);
+	strokeWeight(random(1, 5));
+	point(this.position.x, this.position.y);
+	pop();
+}
+
 
 /* Notes
 *	-rotational inertia for a disk, I = 0.5 * m * r * r 
@@ -32,38 +62,44 @@ function thing(mass, pos, vel) {
 	this.vel = vel;	
 	this.isLocked = false;
 
+	this.radius = Math.sqrt(this.mass / DENSITY / Math.PI);
+
 	this.history = [];
+	// this.ps = new ParticleSystem;
+	this.tps = new ThingParticleSystem(this);
 
 	this.accumulatedForce = createVector(0, 0);
 	this.shouldBeDestroyed = false;
 
 	// public functions
 	this.getRadius = function() {
-		var area = this.mass / DENSITY;
-		var radius = Math.sqrt(area / Math.PI);
-		return radius;
+		return this.radius;
+	};
+
+	this.updateRadius = function() {
+		this.radius = Math.sqrt(this.mass / DENSITY / Math.PI);
 	}
 
-	this.distanceTo = function(otherThing) {
-		var vectorToOther = p5.Vector.sub(this.pos, otherThing.pos);
-		var dist = vectorToOther.mag();
-		return dist;
-	
-	}
+	// this.distanceTo = function(otherThing) {
+	// 	// var vectorToOther = p5.Vector.sub(this.pos, otherThing.pos);
+	// 	// var dist = vectorToOther.mag();
+	// 	// return dist;
+	// 	return (this.pos).dist(otherThing.pos);
+	// };
 	
 	this.toString = function() {
 		var str = "";
 		str += this.pos.toString();
 		return str;
-	}
+	};
 
 	this.accumulateForce = function(force) {
 		this.accumulatedForce.add(force);
-	}
+	};
 	
 	this.toggleLocked = function() {
 		this.isLocked = !this.isLocked;
-	}
+	};
 
 	this.applyAccumulatedForce = function() {
 		var accelarationMag = this.accumulatedForce.mag() / this.mass;
@@ -76,11 +112,11 @@ function thing(mass, pos, vel) {
 		
 		this.vel.add(accelaration);
 		this.accumulatedForce.set(0, 0);
-	}
+	};
 
 	this.isCollidingWith = function(otherThing) {
-		return this.distanceTo(otherThing) <= this.getRadius() + otherThing.getRadius();
-	}
+		return (p5.Vector.sub(this.pos, otherThing.pos)).magSq() <= (this.mass + otherThing.mass) / (DENSITY * PI);
+	};
 
 	this.getGravitationalForce = function(otherThing) {
 		/*
@@ -92,38 +128,44 @@ function thing(mass, pos, vel) {
 		*	m_2 = mass of second object, (otherThing)
 		*	r = distance between m_1 and m_2
 		*/ 
-		var r = this.distanceTo(otherThing);
-		var grav = GRAV * this.mass * otherThing.mass / (r * r);
 		var gravVector = p5.Vector.sub(otherThing.pos, this.pos);
-		gravVector.normalize();
-		gravVector.mult(grav);
-		return gravVector;
-
-	}
+		var rSq = gravVector.magSq();
+		var grav = GRAV * this.mass * otherThing.mass / rSq;
+		return gravVector.normalize().mult(grav);
+	};
 
 	this.getCombinedMomentum = function(otherThing) {
-		// momentum = mass * velocity
+		/*
+		momentum = mass * velocity
+		total momentum = m_1v_1 + m_2v_2
+		*/
 		var myMomentum = p5.Vector.mult(this.vel, this.mass);
 		var theirMomentum = p5.Vector.mult(otherThing.vel, otherThing.mass);		
 		var totalMomentum = p5.Vector.add(myMomentum, theirMomentum);
 		return totalMomentum;
-	}
+	};
 
-	this.absorb = function (otherThing) {
+	this.absorb = function(otherThing) {
 		var totalMomentum = this.getCombinedMomentum(otherThing);
-		this.mass += otherThing.mass;
+		this.mass += otherThing.mass; // new total mass
+		this.updateRadius();
 
-		this.vel = p5.Vector.mult(totalMomentum, 1.0 / this.mass);
+		/*
+		v_after = total momentum / total mass = (m_1v_1 + m_2v_2) / (m_1 + m_2)
+		*/
+		this.vel = p5.Vector.div(totalMomentum, this.mass);
 
 		otherThing.shouldBeDestroyed = true;
-	}
+	};
 
 	this.update = function() {
 		this.updateHistory();
 		this.updatePositionAndAngle();
 		if (bounceEnabled)
 			this.handleEdgeBounce();
-	}
+		// this.ps.addParticle(new Particle(this.pos.copy()));
+		// this.tps.addParticle();
+	};
 
 	this.handleEdgeBounce = function() {
 		var r = this.getRadius();
@@ -132,22 +174,39 @@ function thing(mass, pos, vel) {
 		var w = getZoomedWidth();
 
 		// bounce the balls off the edges of the play area
-		if (this.pos.x - r <= 0 && this.vel.x < 0)
-			this.vel.x *= -BOUNCE_FACTOR;
-		if (this.pos.x >= w - r && this.vel.x > 0)
-			this.vel.x *= -BOUNCE_FACTOR;
-		if (this.pos.y - r <= 0 && this.vel.y < 0)
-			this.vel.y *= -BOUNCE_FACTOR;
-		if (this.pos.y >= h - r && this.vel.y > 0)
-			this.vel.y *= -BOUNCE_FACTOR;
-	}
+		if (this.pos.x <= r && this.vel.x < 0) {
+			this.vel = this.reflect(this.vel, createVector(1, 0));
+			return;
+		}
+			
+		if (this.pos.x >= w - r && this.vel.x > 0) {
+			this.vel = this.reflect(this.vel, createVector(-1, 0));
+			return;
+		}
+			
+		if (this.pos.y <= r && this.vel.y < 0) {
+			this.vel = this.reflect(this.vel, createVector(0, 1));
+			return;
+		}
+			
+		if (this.pos.y >= h - r && this.vel.y > 0) {
+			this.vel = this.reflect(this.vel, createVector(0, -1));
+			return;
+		}
+			
+	};
+
+	this.reflect = function(v, n) {
+		// v - Restitution * v.dot(n) * n;
+		return p5.Vector.sub(v, n.mult(v.copy().dot(n) * 2)).mult(BOUNCE_FACTOR);
+	};
 
 	this.updatePositionAndAngle = function() {
 		if (!this.isLocked) {
 			this.pos.add(p5.Vector.mult(this.vel, TIME_SCALE));
 			this.angle += this.angularVelocity * TIME_SCALE;
 		}
-	}
+	};
 	
 	this.updateHistory = function() {
 		if (SHOW_HISTORY) { // Only calculate if showing history
@@ -158,12 +217,14 @@ function thing(mass, pos, vel) {
 		} else {
 			this.history.length = 0;
 		}
-	}
+	};
 	
 	this.show = function () {
+		// this.ps.run();
+		// this.tps.run();
 		this.showHistory();
 		this.showBody();
-	}
+	};
 
 	this.showBody = function() {
 		var radius = Math.floor(this.getRadius());
@@ -199,6 +260,7 @@ function thing(mass, pos, vel) {
 			
 			// draw the actual "thing"
 			push();
+				noStroke();
 				fill(64);
 				ellipse(0, 0, radius);
 			pop();
@@ -206,6 +268,7 @@ function thing(mass, pos, vel) {
 			// draw rotation indicator
 			push();
 				translate(0, r_indicator);
+				// noStroke();
 				fill(255, 0, 0);
 				ellipse(0, 0, radius * INDICATOR_SIZE_RATIO);
 			pop();
@@ -217,7 +280,7 @@ function thing(mass, pos, vel) {
 				ellipse(0, 0, breathingCenterRadius);
 			pop();
 		pop();
-	}
+	};
 
 	this.showHistory = function() {
 		if (SHOW_HISTORY) {
@@ -226,9 +289,9 @@ function thing(mass, pos, vel) {
 			var alpha = HISTORY_ALPHA;
 			var k = 0.75;
 
+			push();
 			noStroke();
-
-			for (var i = 0; i < this.history.length; i++) {
+			for (var i = 0, length = this.history.length; i < length; i++) {
 				alpha = HISTORY_ALPHA * exp(log(k) * i);				
 				
 				if (alpha <= HISTORY_ALPHA_CUTOFF_THRESHOLD) {
@@ -237,14 +300,15 @@ function thing(mass, pos, vel) {
 
 				// draw history disk
 				push();
-					translate(this.history[i].x, this.history[i].y);
-					fill(red(historyColor), green(historyColor), blue(historyColor), alpha * 255);
-					ellipse(0, 0, (1 - i / this.history.length) * this.getRadius());
+				translate(this.history[i].x, this.history[i].y);
+				fill(red(historyColor), green(historyColor), blue(historyColor), alpha * 255);
+				ellipse(0, 0, (1 - i / this.history.length) * this.getRadius());
 				pop();
 
 			}
+			pop();
 
 		}
-	}
+	};
 
 }
